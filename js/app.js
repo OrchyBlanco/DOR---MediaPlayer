a069c48bdd8c74965c54481c3dd6638416e26eb8
Jejormi <jesus.torres.blanco@outlook.es>
2022-02-12 21:57:59 +0000

    Componente Reproductor Funcional
    Se añade en el componente reproductor Los botones de Play Pause y Stop.
    Ahora al cambiar de canción se para la canción en curso y comienza la seleccionada.

--- js/app.js
+++ js/app.js
@@ -1,37 +1,110 @@
+var audio;
+
 Vue.component("Reproductor", {
-  props: ["datos"],  
-  template: "#reproductor" ,
+  props: {
+    datos:Object
+  },
+   
+  template://html
+  `
   
+  <aside id="aside-reproductor">
+  <button @click="hiddeAside">X</button>
+  <div id="reproductor">
+    
+    <button @click="playSong">Play</button>
+    <button @click="pauseSong">Pause</button>
+    <button @click="stopSong">STOP</button>
+    <img :src="datos.img">
+  </div>
+  <div id="datos">
+    <h1>TITULO: {{datos.name}}</h1>
+    <h2>AUTOR: {{datos.author}}</h2>
+    <p>DESCRIPCION: {{datos.descripcion}}</p>    
+  </div>
+</aside>
+  `,
+methods: {
+    hiddeAside: function(){
+      $("#aside-reproductor").hide();
+      if(audio){
+        audio.pause();
+        audio.currentTime=0;
+      }
+      
+    },
+    playSong:function() {
+        audio.play();
+        
+       },
+    pauseSong:function(){
+      audio.pause();
+    },
+    stopSong:function(){
+      audio.pause();
+      audio.currentTime=0;
+    }
+}
+
 });
 
 Vue.component("Cancion", {
   props: ["cancion"],
   template: "#cancion",
-  methods: {
-    playSong(song) {
-      if (song) {
-        var audio = new Audio(song);
-        audio.play();
+  data: function(){
+      return {
+        name: null,
+        author: null,
+        descripcion: null,
+        img: null,
+        media: null
       }
-    },
-    selectSong(){
-      this.$emit('select',{seleccion: this.cancion})
-    }
+  },
+  methods: {    
+    selectSong:function(){
+      this.$emit('selectSong',{
+        name: this.cancion.name,
+        author: this.cancion.author,
+        descripcion: this.cancion.descripcion,
+        img: this.cancion.img,
+        media: this.cancion.media
+      })
+      if(audio){
+        audio.pause();
+        audio.currentTime=0;
+      }
+      audio=new Audio(this.cancion.media);
+      audio.play();
+      $("#aside-reproductor").show();
+      
+      ;
+    }   
   },
 });
 
 Vue.component("Musica", {
   props: ["musica"],
+  data(){
+    return{
+        cancion_seleccionada:null
+    }        
+},
   template://html
    `
-    <section>
+    <article>
         <Cancion
-        v-for="(cancion,index) in musica" :key="index" :cancion="cancion"
-        
+        v-for="(cancion,index) in musica" 
+        :key="index" 
+        :cancion="cancion" 
+        @selectSong="cancion_seleccionada=$event"
         >
         </Cancion>
-    </section>
+        <span class="d-none">Cancion seleccionada(COMPONENTE MUSICA): {{cancion_seleccionada}}</span>
+    </article>
     `,
+  updated: function(){
+    this.$emit("selectMusica",this.cancion_seleccionada);
+  }
 });
 
 const app = new Vue({
@@ -39,40 +112,54 @@ const app = new Vue({
   data: {
     musica: [
       {
-        name: "qwert",
-        author: "asdf",
-        descripcion: "zxcvb",
-        url: "./media/img/canciones/cancion1.jfif",
-        url_musica: "./media/audio/Only the Braves.mp3",
+        name: "Only the Braves",
+        author: "Desconocido",
+        descripcion: "Cancion sin Copyright",
+        img: "./media/img/canciones/cancion1.jfif",
+        media: "./media/audio/Only the Braves.mp3",
       },
       {
-        name: "asd",
-        author: "qwe",
-        descripcion: "qwerty",
-        url: "./media/img/canciones/DOD_OST_cover.jpg",
-        url_musica: "./media/audio/Drakengard_w_selection.mp3",
+        name: "Weapon Selection",
+        author: "Nobuyoshi Sano, Takayuki Aihara",
+        descripcion: "Drag-on Dragoon Original Soundtrack Vol. 1",
+        img: "./media/img/canciones/DOD_OST_cover.jpg",
+        media: "./media/audio/Drakengard_w_selection.mp3",
       },
       {
-        name: "",
-        author: "",
-        descripcion: "",
-        url: "./media/img/canciones/Darksouls.jpg",
-        url_musica: "./media/audio/Gwyn Lord of Cinder.mp3",
+        name: "Gwyn, Lord of Cinder",
+        author: "Motoi Sakuraba",
+        descripcion: "Dark Souls Original Soundtrack",
+        img: "./media/img/canciones/Darksouls.jpg",
+        media: "./media/audio/Gwyn Lord of Cinder.mp3",
       },
     ],
     videos: [
-      { name: "", descripcion: "", url: "" },
-      { name: "", descripcion: "", url: "" },
+      { name: null, author: null, descripcion: null, img: null, media: null },
+      { name: null, author: null, descripcion: null, img: null, media: null },
+      { name: null, author: null, descripcion: null, img: null, media: null },
     ],
-    seleccion:null,
+    itemSeleccionado: {
+      name: null,
+      author: null,
+      descripcion: null,
+      img: null,
+      media: null,
+    },
   },
+  
   template://html
    `
     <div id="main">    
-      <Reproductor @select="setSeleccion"></Reproductor>
-      <article>
-        <Musica :musica="musica"></Musica>   
-      </article>
+    <Reproductor
+    :datos="itemSeleccionado"
+    ></Reproductor>
+      
+        <Musica 
+        :musica="musica"
+        @selectMusica="itemSeleccionado=$event"
+        ></Musica>   
+        <span class="d-none">Cancion seleccionada(Root): {{this.itemSeleccionado}}</span>
+        
     </div>
     `,
 });
